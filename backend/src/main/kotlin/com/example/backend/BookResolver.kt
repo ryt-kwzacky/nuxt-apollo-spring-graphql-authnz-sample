933e48b50a6b9362c8f40f911f561107c6d9b653
RyutoKawasaki <various.use.0416@gmail.com>
2021-01-04 00:38:58 +0900

    fix resolver

--- backend/src/main/kotlin/com/example/backend/BookResolver.kt
+++ backend/src/main/kotlin/com/example/backend/BookResolver.kt
@@ -1,11 +1,12 @@
 package com.example.backend
 
 import graphql.kickstart.tools.GraphQLQueryResolver
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.stereotype.Component
 
 @Component
 class BookResolver: GraphQLQueryResolver {
     fun bookById(bookId: String): Book {
-        return Book("testId", "nameName")
+        return Book(bookId, RandomStringUtils.randomAlphabetic(20))
     }
 }
